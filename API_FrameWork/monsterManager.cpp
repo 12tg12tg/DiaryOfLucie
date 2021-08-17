b21865bbcbd827a39829638f533409308c893776
KyunGyun <12tg@naver.com>
2021-08-18 01:16:03 +0900

    유령커밋

--- API_FrameWork/monsterManager.cpp
+++ API_FrameWork/monsterManager.cpp
@@ -28,21 +28,21 @@ HRESULT monsterManager::init()
     _yggdrasil = new Cyggdrasil;
     _yggdrasil_bomb = new Cyggdrasil_bomb;
     
-   // _snaby->addMonster(WINSIZEX / 2, 100);
-   /* _slime->addMonster(WINSIZEX / 2, 300);
-    _mushman_mushroom->addMonster(200, 300);
-    _mushman->addMonster(WINSIZEX - 50, 300);
-    _fairy->addMonster(50, 300);
-    _flime->addMonster(WINSIZEX / 2, WINSIZEY / 2);
-    _bossSlime->addMonster(WINSIZEX/2, WINSIZEY/2);
-    _semiBossSlime->addMonster(WINSIZEX/2, WINSIZEY/2);*/
+    //_snaby->addMonster(WINSIZEX / 2, 100);
+    //_slime->addMonster(WINSIZEX / 2, 300);
+    //_mushman_mushroom->addMonster(200, 300);
+    //_mushman->addMonster(WINSIZEX - 50, 300);
+    //_fairy->addMonster(50, 300);
+    //_flime->addMonster(WINSIZEX / 2, WINSIZEY / 2);
+    //_bossSlime->addMonster(WINSIZEX/2, WINSIZEY/2);
+    //_semiBossSlime->addMonster(WINSIZEX/2, WINSIZEY/2);
     //_bossflime->addMonster(WINSIZEX/2, WINSIZEY/2);
-    /*_bossMushmam->addMonster(WINSIZEX/2, WINSIZEY/2);
-    _mushmam_mushroom_G->addMonster(100, 200);
-    _mushmam_mushroom_P->addMonster(100, 300);
-    _mushmam_mushroom_B->addMonster(100, 400);
-    _yggdrasil_bomb->addMonster(WINSIZEX / 2, 100);
-    _yggdrasil->addMonster(WINSIZEX / 2, 100);*/
+    //_bossMushmam->addMonster(WINSIZEX/2, WINSIZEY/2);
+    //_mushmam_mushroom_G->addMonster(100, 200);
+    //_mushmam_mushroom_P->addMonster(100, 300);
+    //_mushmam_mushroom_B->addMonster(100, 400);
+    //_yggdrasil_bomb->addMonster(WINSIZEX / 2, 100);
+    //_yggdrasil->addMonster(WINSIZEX / 2, 100);
     return S_OK;
 }
 
@@ -102,28 +102,9 @@ void monsterManager::update()
     _yggdrasil_bomb->update(_py, _bm);
 
 
-    //static int count = 0;
-    //count++;
+    //Å×½ºÆ®
     if (INPUT->isOnceKeyDown('N')) {
         _mushmam_mushroom_G->addMonster(PLAYER->getPlayerAddress().x, PLAYER->getPlayerAddress().y);
-    //    vector<tagMonster>& v = _yggdrasil->getVMonster();
-    //    auto iter = v.begin();
-    //    for (iter; iter != v.end(); ++iter)
-    //    {
-    //        _yggdrasil->knockback(iter, m_ptMouse.x, m_ptMouse.y, 30, 10, true);
-    //    }
-    //    //vector<tagMonster>& v2 = _semiBossSlime->getVMonster();
-    //    //auto iter2 = v2.begin();
-    //    //for (iter2; iter2 != v2.end(); ++iter2)
-    //    //{
-    //    //    _semiBossSlime->knockback(iter2, m_ptMouse.x, m_ptMouse.y, 10, 10, true);
-    //    //}
-    //    //vector<tagMonster>& v3 = _slime->getVMonster();
-    //    //auto iter3 = v3.begin();
-    //    //for (iter3; iter3 != v3.end(); ++iter3)
-    //    //{
-    //    //    _slime->knockback(iter3, m_ptMouse.x, m_ptMouse.y, 1, 10, true);
-    //    //}
     }
 }
 
