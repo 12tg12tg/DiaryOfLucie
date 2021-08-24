f9b77e7066dd3be49551dc7e070a19defd3ab95c
KyunGyun <12tg@naver.com>
2021-08-24 11:36:14 +0900

    최종수정본 - release exe파일 포함

--- API_FrameWork/camera.h
+++ API_FrameWork/camera.h
@@ -71,29 +71,6 @@ public:
 	void update();
 	void release();
 
-	//피벗반영하여 좌표 당겨서 랜더하기. - 필요없어짐
-	//void RelativeRectangle(HDC hdc, RECT rc);
-	//void RelativeRectangle(HDC hdc, int left, int top, int width, int height);
-	//void RelativeFrameRect(HDC hdc, RECT rc, COLORREF color);
-	//void RelativeLineMake(HDC hdc, int startX, int startY, int endX, int endY);
-	//RECT RelativeRectMake(float x, float y, int width, int height);
-	//void RelativeRender(HDC hdc, image* ig, int destX, int destY);
-	//void RelativeRender(HDC hdc, image* ig, int destX, int destY, int sourX, int sourY, int sourWid, int sourHei);
-	//void RelativeFrameRender(HDC hdc, image* ig, int destX, int destY, int frameX = 0, int frameY = 0);
-	//void RelativeStretchRender(HDC hdc, image* ig, int destX, int destY, float size);
-	//void RelativeStretchRender(HDC hdc, image* ig, int destX, int destY, float scaleX, float scaleY);
-	//void RelativeStretchFrameRender(HDC hdc, image* ig, int destX, int destY, int frameX, int frameY, float size);
-	//void RelativeStretchFrameRender(HDC hdc, image* ig, int destX, int destY, int frameX, int frameY, float scaleX, float scaleY);
-	//void RelativeAlphaRender(HDC hdc, image* ig, int destX, int destY, BYTE alpha);
-	//void RelativeAlphaRender(HDC hdc, image* ig, int destX, int destY, int sourX, int sourY, int sourWid, int sourHei, BYTE alpha);
-	//void RelativeAlphaFrameRender(HDC hdc, image* ig, int destX, int destY, int frameX, int frameY, BYTE alpha);
-	//void RelativeRotateRender(HDC hdc, image* img, int centerX, int centerY, float angle);
-	//void RelativeRotateFrameRender(HDC hdc, image* img, int centerX, int centerY, float angle, int frameX = 0, int frameY = 0);
-	//void RelativeRotateAlphaRender(HDC hdc, image* img, int centerX, int centerY, float angle, BYTE alpha);
-	//void RelativeRotateAlphaFrameRender(HDC hdc, image* img, int centerX, int centerY, float angle, int frameX, int frameY, BYTE alpha);
-	//글씨 당겨서 출력.
-	//void textOut(HDC hdc, int x, int y, const char* text, COLORREF color = RGB(255, 0, 0));
-
 	//페이드인아웃
 	void FadeInit(int time, FADEKIND fadeKind);	
 	void FadeStart();
